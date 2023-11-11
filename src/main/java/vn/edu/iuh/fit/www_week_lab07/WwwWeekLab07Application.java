6b30e854c74fd3960c17dcd128cd732957d13034
DESKTOP-9N2P72A\LaptopAnhHiep <bohiepdzz1@gmail.com>
2023-11-11 13:48:12 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/fit/www_week_lab07/WwwWeekLab07Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week_lab07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwWeekLab07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwWeekLab07Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/www_week_lab07/WwwWeekLab07ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week_lab07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwWeekLab07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
